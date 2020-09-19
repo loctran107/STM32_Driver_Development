b303f57a83aa53301781167b0baac9eea202c229
donavan <dontr710@gmail.com>
2020-09-19 16:54:27 -0700

    Ver 4.6: Complete implementation of USART Send and Receive Datawith tested application

--- STM32F407xx_Drivers/Debug/Src/016StmUSARTArduinoTx.d
+++ STM32F407xx_Drivers/Debug/Src/016StmUSARTArduinoTx.d
@@ -1,5 +1,4 @@
-Src/015MasterArduinoSlaveSTMSendReceive4byte.o: \
- ../Src/015MasterArduinoSlaveSTMSendReceive4byte.c \
+Src/016StmUSARTArduinoTx.o: ../Src/016StmUSARTArduinoTx.c \
  ../Src/../drivers/Inc/stm32f407xx.h \
  ../Src/../drivers/Inc/../Inc/gpio_driver.h \
  ../Src/../drivers/Inc/../Inc/stm32f407xx.h \
--- STM32F407xx_Drivers/Debug/Src/subdir.mk
+++ STM32F407xx_Drivers/Debug/Src/subdir.mk
@@ -4,24 +4,24 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/015MasterArduinoSlaveSTMSendReceive4byte.c \
+../Src/016StmUSARTArduinoTx.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/015MasterArduinoSlaveSTMSendReceive4byte.o \
+./Src/016StmUSARTArduinoTx.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/015MasterArduinoSlaveSTMSendReceive4byte.d \
+./Src/016StmUSARTArduinoTx.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/015MasterArduinoSlaveSTMSendReceive4byte.o: ../Src/015MasterArduinoSlaveSTMSendReceive4byte.c
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DDEBUG -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/015MasterArduinoSlaveSTMSendReceive4byte.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Src/016StmUSARTArduinoTx.o: ../Src/016StmUSARTArduinoTx.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DDEBUG -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/016StmUSARTArduinoTx.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 Src/syscalls.o: ../Src/syscalls.c
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DDEBUG -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/syscalls.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 Src/sysmem.o: ../Src/sysmem.c
